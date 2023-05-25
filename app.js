d6b8a663935c575d37ebeee931f9be9ac5a4d331
Vikram Satyan <vikramsatyan@Vikrams-MacBook-Air.local>
2023-05-24 22:05:55 -0600

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const helloVikram = "Hello Vikram! If you're reading this, you're one step closer to being a 10x dev :D"
 
 console.log(helloVikram)
+
+const helloJosh "Hello Josh! Thank you for setting this up. I am looking forward to becoming a 10x dev"
+
+console.log(helloJosh)
