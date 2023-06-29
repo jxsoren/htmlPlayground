0e49be7a01c899ae2053bdf1154123c2d877632e
Vikram Satyan <vikramsatyan@Vikrams-MacBook-Air.local>
2023-06-28 19:48:46 -0600

    VS updates

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ console.log(helloVikram)
 const helloJosh "Hello Josh! Thank you for setting this up. I am looking forward to becoming a 10x dev"
 
 console.log(helloJosh)
+
+// got the below from Generative AI Google Search Labs
+const object = {
+	key1: "value1"
+	key2: "value2"
+};
+
+const key = "key1";
+
+if (key in object) {
+	console.log(1)
+} else{
+	console.log(2)
+}
