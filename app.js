567ed164dae4064370c584253b39cdc9162d2223
Vikram Satyan <vikramsatyan@Vikrams-MacBook-Air.local>
2023-06-28 20:04:46 -0600

    syntax errors update

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const helloVikram = "Hello Vikram! If you're reading this, you're one step close
 
 console.log(helloVikram)
 
-const helloJosh "Hello Josh! Thank you for setting this up. I am looking forward to becoming a 10x dev"
+const helloJosh = "Hello Josh! Thank you for setting this up. I am looking forward to becoming a 10x dev"
 
 console.log(helloJosh)
 
 // got the below from Generative AI Google Search Labs
 const object = {
-	key1: "value1"
+	key1: "value1",
 	key2: "value2"
 };
 
